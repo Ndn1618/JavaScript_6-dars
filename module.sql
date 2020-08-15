f0d13e376633f0d86ec9dc490e771565aa59bb83
Ndn1618 <ndilbar1618@gmail.com>
2020-08-15 07:21:07 +0300

    Initial Commit

---
+++ module.sql
@@ -0,0 +1,12 @@
+-- SQL language
+
+create database backend_x;
+
+create table users (
+  user_id serial not null primary key,
+  username varchar(32),
+  password varchar(60),
+  name varchar (48)
+);
+
+insert into users (username, password, name) values ('muhammad', 'muhammad1', 'Muhammad Najimov');
